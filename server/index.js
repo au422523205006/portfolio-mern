a66c6cacdd205600f2f9341f088b4d5611412899
raji1209 <rajalakshmiraja2005@gmail.com>
2026-04-29 11:30:48 +0530

    fix cors and port

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ const projectRoutes = require("./routes/projectRoutes");
 const contactRoutes = require("./routes/contactRoutes");
 
 const app = express();
-
 app.use(cors({
-  origin: "https://portfolio-mern-e69423wr9-au422523205006s-projects.vercel.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 app.use(express.json());
 
 
@@ -40,14 +40,13 @@ app.get("/", (req, res) => {
   res.send("Backend working 🚀");
 });
 
-
-// DB
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected ✅"))
   .catch(err => console.log(err));
 
 
-const PORT = 5000;
+
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
