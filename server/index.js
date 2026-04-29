1ca64d69eda7b9665414561fbab95397b15cd459
raji1209 <rajalakshmiraja2005@gmail.com>
2026-04-29 11:39:17 +0530

    fix render port log

--- server/index.js
+++ server/index.js
@@ -41,12 +41,11 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected ✅"))
   .catch(err => console.log(err));
 
-
-
 app.listen(PORT, () => {
-  console.log("Server running on port 5000");
+  console.log(`Server running on port ${PORT}`);
 });
